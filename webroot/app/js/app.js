1dc615ed2d88ecbbb5969b8979a7f7c94880fffb
hantsy <hantsy@gmail.com>
2013-11-12 21:30:36 +0800

    clean the ui, and add profile page

--- webroot/app/js/app.js
+++ webroot/app/js/app.js
@@ -16,6 +16,7 @@
                 .when('/edit-post/:id', {templateUrl: 'partials/edit-post.html', controller: 'EditPostCtrl'})
                 .when('/register', {templateUrl: 'partials/register.html', controller: 'RegisterCtrl'})
                 .when('/login', {templateUrl: 'partials/login.html'})
+                .when('/profile', {templateUrl: 'partials/profile.html'})
                 .otherwise({redirectTo: '/'});
 //        $httpProvider.defaults.useXDomain = true;
 //        delete $httpProvider.defaults.headers.common["X-Requested-With"];
@@ -56,105 +57,106 @@
                         });
 
                 //configure $http to show a login dialog whenever a 401 unauthorized response arrives
-                $httpProvider.responseInterceptors.push(function($rootScope, $q) {
-                    console.log('call response interceptor...');
-                    return function(promise) {
-                        return promise.then(
-                                //success -> don't intercept			
-                                        function(response) {
-                                            console.log('dont intercept...');
-                                            return response;
-                                        },
-                                        //error -> if 401 save the request and broadcast an event
+                $httpProvider.responseInterceptors.push(
+                        function($rootScope, $q) {
+                            console.log('call response interceptor...');
+                            return function(promise) {
+                                return promise.then(
+                                        //success -> don't intercept			
                                                 function(response) {
-                                                    console.log('execute interceptor, response@' + response.status);
-                                                    if (response.status === 401) {
-                                                        console.log('catching http status:401');
-                                                        var deferred = $q.defer(),
-                                                                req = {
-                                                                    config: response.config,
-                                                                    deferred: deferred
-                                                                };
-                                                        $rootScope.requests401.push(req);
-                                                        $rootScope.$broadcast('event:loginRequired');
-                                                        return deferred.promise;
-                                                    }
-                                                    return $q.reject(response);
-                                                }
-                                        );
-                                    };
-                        });
-                httpHeaders = $httpProvider.defaults.headers;
-                //console.log('http headers:'+ httpHeaders);
-            });
-
-            as.run(function($rootScope, $http, $location, base64) {
-                //make current message accessible to root scope and therefore all scopes
-                $rootScope.message = function() {
-                    return message;
-                };
-
-                /**
-                 * Holds all the requests which failed due to 401 response.
-                 */
-                $rootScope.requests401 = [];
-
-                $rootScope.$on('event:loginRequired', function() {
-                    console.log('fire event:loginRequired');
-                    //  $('#login').modal('show');
-                    $location.path('/login');
-                });
-
-                /**
-                 * On 'event:loginConfirmed', resend all the 401 requests.
-                 */
-                $rootScope.$on('event:loginConfirmed', function() {
-                    var i,
-                            requests = $rootScope.requests401,
-                            retry = function(req) {
-                                $http(req.config).then(function(response) {
-                                    req.deferred.resolve(response);
+                                                    console.log('dont intercept...');
+                                                    return response;
+                                                },
+                                                //error -> if 401 save the request and broadcast an event
+                                                        function(response) {
+                                                            console.log('execute interceptor, response@' + response.status);
+                                                            if (response.status === 401) {
+                                                                console.log('catching http status:401');
+                                                                var deferred = $q.defer(),
+                                                                        req = {
+                                                                            config: response.config,
+                                                                            deferred: deferred
+                                                                        };
+                                                                $rootScope.requests401.push(req);
+                                                                $rootScope.$broadcast('event:loginRequired');
+                                                                return deferred.promise;
+                                                            }
+                                                            return $q.reject(response);
+                                                        }
+                                                );
+                                            };
                                 });
-                            };
+                        httpHeaders = $httpProvider.defaults.headers;
+                        //console.log('http headers:'+ httpHeaders);
+                    });
 
-                    for (i = 0; i < requests.length; i += 1) {
-                        retry(requests[i]);
-                    }
-                    $rootScope.requests401 = [];
+                    as.run(function($rootScope, $http, $location, base64) {
+                        //make current message accessible to root scope and therefore all scopes
+                        $rootScope.message = function() {
+                            return message;
+                        };
 
-                    $location.path('/');
-                });
+                        /**
+                         * Holds all the requests which failed due to 401 response.
+                         */
+                        $rootScope.requests401 = [];
 
-                /**
-                 * On 'event:loginRequest' send credentials to the server.
-                 */
-                $rootScope.$on('event:loginRequest', function(event, username, password) {
-                    //            httpHeaders.common['Authorization'] = 'Basic ' + base64.encode(username + ':' + password);
-                    //            $http.get('action/user').success(function (data) {
-                    //                $rootScope.user = data;
-                    //                $rootScope.$broadcast('event:loginConfirmed');
-                    //            });
-                    console.log('fire event: loginRequest. @event,' + event + ', username @' + username + ', password@' + password);
-                    httpHeaders.common['Authorization'] = 'Basic ' + base64.encode(username + ':' + password);
-                    $http.get($rootScope.appUrl + '/users/login.json')
-                            .success(function(data) {
-                                console.log('login data @' + data);
-                                $rootScope.user = data.user;
-                                $rootScope.$broadcast('event:loginConfirmed');
-                            });
+                        $rootScope.$on('event:loginRequired', function() {
+                            console.log('fire event:loginRequired');
+                            //  $('#login').modal('show');
+                            $location.path('/login');
+                        });
 
-                });
+                        /**
+                         * On 'event:loginConfirmed', resend all the 401 requests.
+                         */
+                        $rootScope.$on('event:loginConfirmed', function() {
+                            var i,
+                                    requests = $rootScope.requests401,
+                                    retry = function(req) {
+                                        $http(req.config).then(function(response) {
+                                            req.deferred.resolve(response);
+                                        });
+                                    };
 
-                /**
-                 * On 'logoutRequest' invoke logout on the server and broadcast 'event:loginRequired'.
-                 */
-                $rootScope.$on('event:logoutRequest', function() {
-                    $http.get($rootScope.appUrl + '/users/logout.json')
-                            .success(function(data) {
-                                httpHeaders.common['Authorization'] = null;
-                            });
+                            for (i = 0; i < requests.length; i += 1) {
+                                retry(requests[i]);
+                            }
+                            $rootScope.requests401 = [];
 
-                });
-            });
+                            $location.path('/');
+                        });
 
-        }());
+                        /**
+                         * On 'event:loginRequest' send credentials to the server.
+                         */
+                        $rootScope.$on('event:loginRequest', function(event, username, password) {
+                            //            httpHeaders.common['Authorization'] = 'Basic ' + base64.encode(username + ':' + password);
+                            //            $http.get('action/user').success(function (data) {
+                            //                $rootScope.user = data;
+                            //                $rootScope.$broadcast('event:loginConfirmed');
+                            //            });
+                            console.log('fire event: loginRequest. @event,' + event + ', username @' + username + ', password@' + password);
+                            httpHeaders.common['Authorization'] = 'Basic ' + base64.encode(username + ':' + password);
+                            $http.get($rootScope.appUrl + '/users/login.json')
+                                    .success(function(data) {
+                                        console.log('login data @' + data);
+                                        $rootScope.user = data.user;
+                                        $rootScope.$broadcast('event:loginConfirmed');
+                                    });
+
+                        });
+
+                        /**
+                         * On 'logoutRequest' invoke logout on the server and broadcast 'event:loginRequired'.
+                         */
+                        $rootScope.$on('event:logoutRequest', function() {
+                            $http.get($rootScope.appUrl + '/users/logout.json')
+                                    .success(function(data) {
+                                        httpHeaders.common['Authorization'] = null;
+                                    });
+
+                        });
+                    });
+
+                }());
